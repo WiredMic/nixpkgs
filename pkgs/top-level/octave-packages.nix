79333a169a1543edc9bbcb4b21cea3bdb54ec321
Rasmus Enevoldsen <rasmus@enev.dk>
2026-03-13 19:32:06 +0100

    octavePackages.bioinfo: init at 0.2.1

--- pkgs/top-level/octave-packages.nix
+++ pkgs/top-level/octave-packages.nix
@@ -70,6 +70,8 @@ makeScope newScope (
 
     bim = callPackage ../development/octave-modules/bim { };
 
+    bioinfo = callPackage ../development/octave-modules/bioinfo { };
+
     biosig = callPackage ../development/octave-modules/biosig {
       # PLACEHOLDER until biosig gets packaged.
       biosig = null;
