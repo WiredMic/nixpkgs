f9dd403e18796d9ef44330c6eb7e515a0df58ca3
Rasmus Enevoldsen <rasmus@enev.dk>
2026-05-19 22:14:23 +0200

    spellchecker-cli: init at 7.0.3

---
+++ pkgs/by-name/sp/spellchecker-cli/package.nix
@@ -0,0 +1,46 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  fetchYarnDeps,
+  yarnConfigHook,
+  yarnBuildHook,
+  yarnInstallHook,
+  nodejs,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "spellchecker-cli";
+  version = "7.0.3";
+
+  strictDeps = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "tbroadley";
+    repo = "spellchecker-cli";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-4rKUxXsZKsRDhMV0HL39yQyVNI0negCg97KsI+77oI4=";
+  };
+
+  yarnOfflineCache = fetchYarnDeps {
+    yarnLock = finalAttrs.src + "/yarn.lock";
+    hash = "sha256-GWIjk8eV2yYwsAfe7IY2mjO/dk9mb4vXEOvp68y4eMk=";
+  };
+
+  nativeBuildInputs = [
+    yarnConfigHook
+    yarnBuildHook
+    yarnInstallHook
+    nodejs
+  ];
+
+  yarnBuildScript = "prepack";
+
+  meta = {
+    description = "A command-line tool for spellchecking files";
+    homepage = "https://www.npmjs.com/package/spellchecker-cli";
+    mainProgram = "spellchecker";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ WiredMic ];
+  };
+})
